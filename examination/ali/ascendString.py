030d603a149430478ff59f111a81bc09926577c2
Solarzhou <t-zhou@foxmail.com>
2020-08-19 16:59:42 +0800

    update

--- examination/ali/ascendString.py
+++
@@ -1,6 +0,0 @@
-'''
-@Time    : 2020/3/21 20:05
-@FileName: ascendString.py
-@Author  : Solarzhou
-@Email   : t-zhou@foxmail.com
-'''
--- examination/kuaishou/numberValue.py
+++
@@ -1,6 +0,0 @@
-'''
-@Time    : 2020/3/22 20:10
-@FileName: numberValue.py
-@Author  : Solarzhou
-@Email   : t-zhou@foxmail.com
-'''
